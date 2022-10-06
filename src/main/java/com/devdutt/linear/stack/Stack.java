deacf09f8299164856765c94d1da0c75aee7007b
javabydevdutt <javabydevdutt@gmail.com>
2022-10-06 18:49:06 +0530

    size(),isFull(),isEmpty(),peek() method implemented

--- src/main/java/com/devdutt/linear/stack/Stack.java
+++ src/main/java/com/devdutt/linear/stack/Stack.java
@@ -14,9 +14,17 @@ public class Stack {
         this.arr = new int[capacity];
     }
 
+    public boolean isFull() {
+        return (top + 1) == capacity;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
     //Operation:01- Insertion
     public void push(int data) {
-        if (top + 1 == capacity) {
+        if (isFull()) {
             System.out.println("Stack is full can't inserted");
             return;
         }
@@ -27,33 +35,56 @@ public class Stack {
         System.out.println(data + " is inserted in the stack");
     }
 
-    //delete the element from the stack
+    //Operation:02- Deletion
     public void pop() {
-        if (top == -1) {
+        if (isEmpty()) {
             System.out.println("Stack is empty");
             return;
         }
 
         //place the arr[top] element in the data
         int data = arr[top];
-        //decrement the top by 1
+        //decrent the top by 1
         top = top - 1;
         System.out.println(data + " is removed from the stack");
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[top];
+    }
+
+    public int size() {
+        return top + 1;
     }
 
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.push(20);
         stack.push(30);
+        System.out.println("stack size:- " + stack.size());
         stack.push(50);
         stack.push(10);
         stack.push(40);
         stack.push(70);
+        System.out.println("stack size:- " + stack.size());
+        System.out.println("Before Removing Element");
+        System.out.println("Is Stack is Full:- " + stack.isFull());
+        System.out.println("Is Stack is Empty:- " + stack.isEmpty());
+        System.out.println(stack.peek() + " peek from the stack");
+        System.out.println("======================");
+        System.out.println("After Removing Element");
+        stack.pop();
+        System.out.println("stack size:- " + stack.size());
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
-        stack.pop();
+        System.out.println("stack size:- " + stack.size());
+        System.out.println("Is Stack is Full:- " + stack.isFull());
+        System.out.println("Is Stack is Empty:- " + stack.isEmpty());
     }//main
 }//class
